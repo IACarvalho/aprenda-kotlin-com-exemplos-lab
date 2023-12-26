d3d0dd7ada1d3527dd00c9d3ef83cdfca79d59a2
Israel Afonso de Carvalho Neto <israelafonsodecarvalhoneto@Israels-MacBook-Pro.local>
2023-12-26 20:10:57 -0300

    Modificacoes no desafio da DIO

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,37 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val cursosConcluidos: MutableList<ConteudoEducacional> = mutableListOf())
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, nivel: Nivel)) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun concluiu(usuario: Usuario, conteudo: ConteudoEducacional) {
+        usuario.cursosConcluidos.add(conteudo)
+
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val alunoJoao = Usuario("João")
+    val alunoMaria = Usuario("Maria")
+    val alunoJose = Usuario("José")
+
+    val cursoKotlinBasico = ConteudoEducacional("Kotlin Básico")
+    val cursoKotlinIntermediario = ConteudoEducacional("Kotlin Intermediário")
+    val cursoKotlinAvancado = ConteudoEducacional("Kotlin Avançado")
+
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(cursoKotlinBasico, cursoKotlinIntermediario, cursoKotlinAvancado), Nivel.DIFICIL)
+    formacaoKotlin.matricular(alunoJoao)
+    formacaoKotlin.matricular(alunoMaria)
+    formacaoKotlin.matricular(alunoJose)
+
+    formacaoKotlin.concluiu(alunoJoao, cursoKotlinBasico)
 }
